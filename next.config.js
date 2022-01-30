2b667ce295b176ac309168e609401ddf358aeac8
HendrickSamuel <samuel@certinergie.be>
2022-01-30 15:35:19 +0100

    :sparkles: Add navbar

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  devIndicators: {
+    buildActivity: true,
+    buildActivityPosition: 'bottom-right',
+    autoPrerender: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styles from './navbar.module.css';
+import Head from 'next/head';
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+import { BiAtom } from 'react-icons/bi';
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li>
+          <h1>
+            <a href="">
+              <span>
+                <BiAtom className={styles.main_icon} />
+              </span>
+              <span>Hendrick Samuel</span>
+            </a>
+          </h1>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">
+            <FaLinkedin aria-hidden="true" className={styles.icon} />
+            <span className="sr-only">Linkedin</span>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <FaGithubSquare aria-hidden="true" className={styles.icon} />
+            <span className="sr-only">Github</span>
+          </a>
+        </li>
+        <li>
+          <a href="">Resume</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
